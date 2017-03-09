3b2d1bcfd3680120555c1f1f2071e98795aa3f9a
Anthony Prudhomme <anthony@MacBook-Pro-de-Anthony.local>
2017-03-09 15:16:42 +0100

    First commit

---
+++ MovieDatas/src/firstapplication/Movie.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firstapplication;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author anthony
+ */
+public class Movie {
+    String title;
+    int releaseYear;
+    String genre;
+    int duration;
+    Director director;
+    ArrayList<Actor> actors;
+    double imdbScore;
+    int gross;
+    int budget;
+    String country;
+    String language;
+    boolean colored;
+    String linkToInformation;
+    ArrayList<String> plotKeywords;
+    
+    public Movie(String title, int releaseYear, String genre, int duration, Director director, ArrayList<Actor> actors, double imdbScore, int gross, int budget, String country, String language, boolean colored, String linkToInformation, ArrayList<String> plotKeywords) {
+        this.title = title;
+        this.releaseYear = releaseYear;
+        this.genre = genre;
+        this.duration = duration;
+        this.director = director;
+        this.actors = actors;
+        this.imdbScore = imdbScore;
+        this.gross = gross;
+        this.budget = budget;
+        this.country = country;
+        this.language = language;
+        this.colored = colored;
+        this.linkToInformation = linkToInformation;
+        this.plotKeywords = plotKeywords;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public Director getDirector() {
+        return director;
+    }
+
+    public ArrayList<Actor> getActors() {
+        return actors;
+    }
+
+    public double getImdbScore() {
+        return imdbScore;
+    }
+
+    public int getGross() {
+        return gross;
+    }
+
+    public int getBudget() {
+        return budget;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public boolean isColored() {
+        return colored;
+    }
+
+    public String getLinkToInformation() {
+        return linkToInformation;
+    }
+
+    public ArrayList<String> getPlotKeywords() {
+        return plotKeywords;
+    }
+}
